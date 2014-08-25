ce69e550a87d33d5a65cc2dd19262f5fa3897f92
Scott <sdc50@byu.net>
2014-08-25 16:35:10 -0600

    add coverage testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,15 @@
-import unittest
+import unittest, coverage
 
 def runall():
     allTests = unittest.TestLoader().discover('')
     unittest.TextTestRunner(verbosity=2).run(allTests)
 
 if __name__ == "__main__":
+    cov = coverage.coverage(branch=True, source=['./condorpy'])
+    cov.start()
+
     runall()
+
+    cov.stop()
+    cov.save()
+    cov.html_report()
