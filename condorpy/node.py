b097115895b283821b66061a321a77560270b813
Scott <sdc50@byu.net>
2015-03-18 14:01:17 -0600

    documentation

--- condorpy/node.py
+++ condorpy/node.py
@@ -4,7 +4,7 @@
 #
 # condorpy is free software: you can redistribute it and/or modify it under
 # the terms of the BSD 2-Clause License. A copy of the BSD 2-Clause License
-# should have be distributed with this file.
+# should have been distributed with this file.
 
 from job import Job
 
