1b0f12a7f3832cb20d28c1695a91f93e098f9757
Scott <sdc50@byu.net>
2015-03-21 08:34:53 -0600

    change back to sphinx napoleon extension, but add requirements file to work with read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
-    # 'sphinx.ext.napoleon',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
